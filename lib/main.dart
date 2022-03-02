a2a5989d75176aea0d8f0b5622674b6f33876ffa
Alex Ushkov <alexushkov82@gmail.com>
2022-03-02 10:29:45 +0300

    Playground for 37h of Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'timetable.dart';
-import 'timetable_detail.dart';
 
 void main() {
   runApp(const App());
@@ -9,101 +7,61 @@ void main() {
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Kickstartmyheart App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark(),
-        home: MyHomePage(title: 'Timetable'));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            widget.title,
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w900,
-            ),
-          ),
-          ),
-        ),
-      body: SafeArea(
-        child: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: Timetable.samples.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return TimetableDetail(timetable: Timetable.samples[index]);
-                    },
-                  ),
-                );
-              },
-              child: buildTimetableCard(Timetable.samples[index]),
-            );
-          },
-        ),
+        title: Text(widget.title),
       ),
-    );
-  }
-
-  Widget buildTimetableCard(Timetable timetable) {
-    return Card(
-      elevation: 2,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-
-      child: Padding(
-        padding: const EdgeInsets.all(16),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              timetable.name,
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Image(image: AssetImage(timetable.image)),
-            const SizedBox(
-              height: 15,
+            const Text(
+              'You have pushed the button this many times:',
             ),
             Text(
-              timetable.time,
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-              ),
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
-
 }
