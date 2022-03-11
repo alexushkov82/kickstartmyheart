9347ccd62ba0b34befa386f96e578c0c4e19cbfa
Alex Ushkov <alexushkov82@gmail.com>
2022-03-11 14:03:16 +0300

    GL 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    ),
+  );
 }
 
 class HomePage extends StatefulWidget {
@@ -49,7 +42,7 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You\'ve pushed the button this many times:',
             ),
             Text(
               '$_counter',
