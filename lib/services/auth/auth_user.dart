4ea4ece12880ae0cd126f591ce6d88494718379c
Alex Ushkov <alexushkov82@gmail.com>
2022-03-21 13:08:25 +0300

    37hF services/auth folder

---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart' show User;
+
+
+@immutable
+class AuthUser {
+  final bool isEmailVerified;
+
+  const AuthUser(this.isEmailVerified);
+
+  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+
+}
